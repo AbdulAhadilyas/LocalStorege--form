c5ea57745847450047f89f88d04e423f2e87b906
Abdul Ahad <ahadmemon3340@gmail.com>
2022-07-22 18:36:14 +0500

    working

---
+++ js/app.js
@@ -0,0 +1,93 @@
+const form = document.getElementById("form");
+const username = document.getElementById("username");
+const email = document.getElementById("email");
+const password = document.getElementById("password");
+const password2 = document.getElementById("password2");
+
+function showError(input, message) {
+  const formControl = input.parentElement;
+  formControl.className = "form-control error";
+  const small = formControl.querySelector("small");
+  small.innerText = message;
+}
+
+
+
+function checkEmail(input) {
+  const re =
+    /^(([^<>()\[\]\\.,;:\s@"]+(\.[^<>()\[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/;
+  if (re.test(input.value.trim())) {
+    showSuccess(input);
+  } else {
+    showError(input, `Please provide a valid email`);
+  }
+}
+
+function showSuccess(input) {
+  const formControl = input.parentElement;
+  formControl.className = "form-control success";
+}
+
+function checkRequired(inputArray) {
+  inputArray.forEach(function (input) {
+    if (input.value === "") {
+      console.log(input.id);
+      showError(input, `${getFieldId(input)} is required`);
+    } else {
+      showSuccess(input);
+    }
+  });
+}
+
+function checkPasswordsMatch(input1, input2) {
+  if (input1.value !== input2.value) {
+    showError(input2, "Passwords don't match");
+  }
+}
+
+function getFieldId(input) {
+  return input.id.charAt(0).toUpperCase() + input.id.slice(1);
+}
+
+function onSingup() {
+  let newUser = {
+    name: username.value,
+    email: email.value,
+    password: password.value,
+    confirm_password: password2.value,
+  };
+
+  let users = [];
+
+  let userInStringForm = localStorage.getItem("users");
+  users = JSON.parse(userInStringForm) || [];
+
+  var userChk = users.findIndex(function (val) {
+    return val.email.toLowerCase() === newUser.email.toLowerCase();
+  });
+  console.log(userChk);
+
+  if (userChk == -1) {
+    users.push(newUser);
+    localStorage.setItem("users", JSON.stringify(users));
+    window.location.href = "./html/login.html";
+  } else {
+    console.log("älready in use");
+    showError(email, "This Email Is Already In Use");
+  }
+}
+
+form.addEventListener("submit", function (e) {
+  e.preventDefault();
+  checkRequired([username, password, password2]);
+  checkEmail(email);
+  checkPasswordsMatch(password, password2);
+  onSingup();
+});
+
+
+
+
+
+
+
---
+++ js/login.js
@@ -0,0 +1,84 @@
+
+const form = document.getElementById("form");
+const email = document.getElementById("email");
+const password = document.getElementById("password");
+
+function showError(input, message) {
+  const formControl = input.parentElement;
+  formControl.className = "form-control error";
+  const small = formControl.querySelector("small");
+  small.innerText = message;
+}
+
+function showSuccess(input) {
+  const formControl = input.parentElement;
+  formControl.className = "form-control success";
+}
+function checkEmail(input) {
+  const re =
+    /^(([^<>()\[\]\\.,;:\s@"]+(\.[^<>()\[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/;
+  if (re.test(input.value.trim())) {
+    showSuccess(input);
+  } else {
+    showError(input, `Please provide a valid email`);
+  }
+}
+
+function checkPasswords(input1) {
+    if (input1.value == "") {
+      showError(input1, "Passwords don't match");
+    }
+  }
+  
+function getFieldId(input) {
+  return input.id.charAt(0).toUpperCase() + input.id.slice(1);
+}
+function onLogin(){
+
+  let newUser = {
+      email: email.value,
+      password: password.value,
+    };
+
+    let users = [];
+
+let userInStringForm = localStorage.getItem("users");
+users = JSON.parse(userInStringForm) || [];
+
+var userChk = users.find(function (cheak) {
+  return cheak.email.toLowerCase() === newUser.email.toLowerCase() && cheak.password.toLowerCase() ===newUser.password.toLowerCase() 
+});
+console.log(userChk);
+
+if (userChk) {
+  localStorage.setItem("user", JSON.stringify(userChk));
+  window.location.href = "./home.html";
+} else {
+  showError(email, `worng email`);
+  showError(password, `worng password`);
+}
+setTimeout(() => {
+  message.innerHTML = "";
+}, 1000)
+
+}
+form.addEventListener("submit", function (e) {
+  e.preventDefault();
+  checkEmail(email);
+  checkPasswords(password)
+  onLogin()
+});
+
+function onLogout() {
+  localStorage.removeItem("user");
+  setTimeout(() => {
+      location.href = "./login.html";
+  }, 1000);
+}
+
+function toGetUser(){
+ let user1 = localStorage.getItem("user")
+ user1 = JSON.parse(user1)
+ document.getElementById('box').innerHTML= "Wellcome : " + user1.email
+}
+
